a33fa26910e4ee73882d3371b5e579e7c0b2c134
Mateusz Gumieniak <mateusz.gumieniak@wp.pl>
2019-05-18 22:06:04 +0200

    Create class User, Product, Cloth and Boots

--- src/Boots.java
+++ src/Boots.java
@@ -1,4 +1,27 @@
 public class Boots extends Product{
-    int size;
-    boolean isNaturalSkin = true;
+    private int size;
+    private boolean isNaturalSkin = true;
+
+    public Boots (long id, String productName, float price, float weight, String color, int productCount, int size,
+                  boolean isNaturalSkin)
+    {
+        super(id, productName, price, weight, color, productCount);
+        this.size = size;
+        this.isNaturalSkin = isNaturalSkin;
+    }
+
+    public int getSize()
+    {
+        return size;
+    }
+
+    public boolean isNaturalSkin()
+    {
+        return isNaturalSkin;
+    }
+    @Override
+    public String toString()
+    {
+        return super.toString() + "Boots{" + "size='" + size + '\'' + ", isNaturalSkin=" + isNaturalSkin + '}';
+    }
 }
--- src/User.java
+++ src/User.java
@@ -1,12 +1,29 @@
 public class User {
-    String id;
-    String login;
-    String password;
+    private long id;
+    private String login;
+    private String password;
 
-    public User (String id, String login, String password)
-    {
+    public User(long id, String login, String password) {
         this.id = id;
         this.login = login;
         this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "User{" + "id=" + id + ", login='" + login + '\'' + ", password='" + password + '\'' + '}';
     }
 }
